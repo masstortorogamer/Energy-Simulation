5b483d4fac1da151a376395ba28b770730650132
masstortorogamer <59394749+masstortorogamer@users.noreply.github.com>
2020-01-14 15:07:11 +0300

    WIP

---
+++ Assets/Scripts/Live.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Live : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float powerUsage = 0.5f;
+
+    private int time;
+    void Start()
+    {
+        time = PassTime.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        switch(time)
+        {
+            case 1:
+                powerUsage = Random.Range(0.1f, 1f);
+                GameObject.Find("B1").GetComponent<RectTransform>().localScale = new Vector3(0.1838f, powerUsage, 0);
+                break;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PassTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassTime : MonoBehaviour
+{
+    public static int time = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Time()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(120);
+            time += 1;
+        }
+        
+    }
+}
